1bbefd409baf7cf222f4677fcb316b3aa51b0235
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2018-12-31 14:39:25 +0100

    Share the same graph and use it as default

--- bnn/tensorflow/input_pipeline.py
+++ bnn/tensorflow/input_pipeline.py
@@ -1,52 +1,57 @@
 import tensorflow as tf
 
 
-def build_input_pipeline(X, y, batch_size, heldout_size):
+def build_input_pipeline(X, y, batch_size, heldout_size, graph):
     """Build an Iterator switching between train and heldout data."""
 
-    # Build an iterator over training batches.
-    training_dataset = tf.data.Dataset.from_tensor_slices((X, y))
-    training_batches = training_dataset.shuffle(
-        len(X), reshuffle_each_iteration=True).repeat().batch(batch_size)
-    training_iterator = training_batches.make_one_shot_iterator()
+    with graph.as_default():
+        # Build an iterator over training batches.
+        training_dataset = tf.data.Dataset.from_tensor_slices((X, y))
+        training_batches = training_dataset.shuffle(
+            len(X), reshuffle_each_iteration=True).repeat().batch(batch_size)
+        training_iterator = training_batches.make_one_shot_iterator()
 
-    # Build a iterator over the heldout set with batch_size=heldout_size,
-    # i.e., return the entire heldout set as a constant.
-    heldout_dataset = tf.data.Dataset.from_tensor_slices((X, y))
-    heldout_frozen = (
-        heldout_dataset.take(heldout_size).repeat().batch(batch_size))
-    heldout_iterator = heldout_frozen.make_one_shot_iterator()
+        # Build a iterator over the heldout set with batch_size=heldout_size,
+        # i.e., return the entire heldout set as a constant.
+        heldout_dataset = tf.data.Dataset.from_tensor_slices((X, y))
+        heldout_frozen = (
+            heldout_dataset.take(heldout_size).repeat().batch(batch_size))
+        heldout_iterator = heldout_frozen.make_one_shot_iterator()
 
-    # Combine these into a feedable iterator that can switch between training
-    # and validation inputs.
-    handle = tf.placeholder(tf.string, shape=[])
-    feedable_iterator = tf.data.Iterator.from_string_handle(
-        handle, training_batches.output_types, training_batches.output_shapes)
-    X, y = feedable_iterator.get_next()
+        # Combine these into a feedable iterator that can switch between training
+        # and validation inputs.
+        handle = tf.placeholder(tf.string, shape=[])
+        feedable_iterator = tf.data.Iterator.from_string_handle(
+            handle, training_batches.output_types,
+            training_batches.output_shapes)
+        X, y = feedable_iterator.get_next()
 
-    return X, y, handle, training_iterator, heldout_iterator
+    return X, y, handle, training_iterator, heldout_iterator, graph
 
 
-def build_input_val_pipeline(X, y, Xval, yval, batch_size, batch_heldout_size):
+def build_input_val_pipeline(X, y, Xval, yval, batch_size, batch_heldout_size,
+                             graph):
     """Build an Iterator switching between train and heldout data."""
 
-    # Build an iterator over training batches.
-    training_dataset = tf.data.Dataset.from_tensor_slices((X, y))
-    training_batches = training_dataset.shuffle(
-        len(X), reshuffle_each_iteration=True).repeat().batch(batch_size)
-    training_iterator = training_batches.make_one_shot_iterator()
+    with graph.as_default():
+        # Build an iterator over training batches.
+        training_dataset = tf.data.Dataset.from_tensor_slices((X, y))
+        training_batches = training_dataset.shuffle(
+            len(X), reshuffle_each_iteration=True).repeat().batch(batch_size)
+        training_iterator = training_batches.make_one_shot_iterator()
 
-    # Build a iterator over the heldout set with batch_size=heldout_size,
-    # i.e., return the entire heldout set as a constant.
-    heldout_dataset = tf.data.Dataset.from_tensor_slices((Xval, yval))
-    heldout_frozen = (heldout_dataset.repeat().batch(batch_heldout_size))
-    heldout_iterator = heldout_frozen.make_one_shot_iterator()
+        # Build a iterator over the heldout set with batch_size=heldout_size,
+        # i.e., return the entire heldout set as a constant.
+        heldout_dataset = tf.data.Dataset.from_tensor_slices((Xval, yval))
+        heldout_frozen = (heldout_dataset.repeat().batch(batch_heldout_size))
+        heldout_iterator = heldout_frozen.make_one_shot_iterator()
 
-    # Combine these into a feedable iterator that can switch between training
-    # and validation inputs.
-    handle = tf.placeholder(tf.string, shape=[])
-    feedable_iterator = tf.data.Iterator.from_string_handle(
-        handle, training_batches.output_types, training_batches.output_shapes)
-    X, y = feedable_iterator.get_next()
+        # Combine these into a feedable iterator that can switch between training
+        # and validation inputs.
+        handle = tf.placeholder(tf.string, shape=[])
+        feedable_iterator = tf.data.Iterator.from_string_handle(
+            handle, training_batches.output_types,
+            training_batches.output_shapes)
+        X, y = feedable_iterator.get_next()
 
-    return X, y, handle, training_iterator, heldout_iterator
+    return X, y, handle, training_iterator, heldout_iterator, graph
--- bnn/tensorflow/models.py
+++ bnn/tensorflow/models.py
@@ -10,96 +10,98 @@ def sample_d(d):
     return tf.reduce_mean(d.sample(pred_samples, 0))
 
 
-def make_conv_net():
-    model = tf.keras.Sequential([
-        tf.keras.layers.Reshape([8, 15, 10]),
-        tfp.layers.Convolution2DFlipout(
-            120,
-            kernel_size=2,
-            padding='SAME',
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        # tf.keras.layers.MaxPooling2D(pool_size=[2,2], strides=[2,2], padding='SAME'),
-        tfp.layers.Convolution2DFlipout(
-            60,
-            kernel_size=3,
-            padding='SAME',
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPooling2D(
-            pool_size=[2, 2], strides=[2, 2], padding='SAME'),
-        tf.keras.layers.Flatten(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            1,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-    ])
-    return model
+def make_conv_net(graph):
+    with graph.as_default():
+        model = tf.keras.Sequential([
+            tf.keras.layers.Reshape([8, 15, 10]),
+            tfp.layers.Convolution2DFlipout(
+                120,
+                kernel_size=2,
+                padding='SAME',
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            # tf.keras.layers.MaxPooling2D(pool_size=[2,2], strides=[2,2], padding='SAME'),
+            tfp.layers.Convolution2DFlipout(
+                60,
+                kernel_size=3,
+                padding='SAME',
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.MaxPooling2D(
+                pool_size=[2, 2], strides=[2, 2], padding='SAME'),
+            tf.keras.layers.Flatten(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                1,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+        ])
+    return model, graph
 
 
-def make_mlp_net():
-    model = tf.keras.Sequential([
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            10,
-            activation=tf.nn.relu,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-        tf.keras.layers.BatchNormalization(),
-        tfp.layers.DenseFlipout(
-            1,
-            kernel_posterior_tensor_fn=sample_d,
-            bias_posterior_tensor_fn=sample_d),
-    ])
-    return model
+def make_mlp_net(graph):
+    with graph.as_default():
+        model = tf.keras.Sequential([
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                10,
+                activation=tf.nn.relu,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+            tf.keras.layers.BatchNormalization(),
+            tfp.layers.DenseFlipout(
+                1,
+                kernel_posterior_tensor_fn=sample_d,
+                bias_posterior_tensor_fn=sample_d),
+        ])
+    return model, graph
